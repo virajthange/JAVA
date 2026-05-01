b4056b779c025df10f6e0129151d00b7e829ba81
Viraj <virajthange45@gmail.com>
2026-05-01 19:46:05 +0530

    Adding Object class

---
+++ src/objectClass/Aura.java
@@ -0,0 +1,17 @@
+package objectClass;
+
+class Power extends Object{
+
+}
+
+
+public class Aura {
+    public static void main(String[] args) {
+        Object val = 43 , name = "Sanket";
+
+        System.out.println(val);
+        System.out.println(name);
+
+        Power power = new Power();
+    }
+}
---
+++ src/wrapperClasses/IntegerClass.java
@@ -0,0 +1,8 @@
+package wrapperClasses;
+
+public class IntegerClass {
+    public static void main(String[] args) {
+        Integer i1 = Integer.valueOf("432");
+        System.out.println(i1);
+    }
+}
